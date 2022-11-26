d89f411ff2c36b4cbde1579b8e9620f747756aef
Jackie Lai <jackie12051995@gmail.com>
2022-11-27 10:58:39 +1300

    Modify Day 28

--- RegExPatternsAndIntroToDatabases.py
+++ RegExPatternsAndIntroToDatabases.py
@@ -14,16 +14,13 @@ def email_filter(table):
 
 
 if __name__ == '__main__':
-    N = 6
-    data = ["riya riya@gmail.com", "julia julia@julia.me", "julia sjulia@gmail.com", "julia julia@gmail.com",
-            "samantha samantha@gmail.com", "tanya tanya@gmail.com"]
-    database = {}
+    N = int(input().strip())
+    database = dict()
     for N_itr in range(N):
-        first_multiple_input = data[N_itr].split()
+        first_multiple_input = input().rstrip().split()
         firstName = first_multiple_input[0]
         emailID = first_multiple_input[1]
         database[emailID] = firstName
-    print(database)
     email_filter(database)
 
 
